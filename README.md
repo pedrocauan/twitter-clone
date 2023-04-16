8b9e042054074670640ab52651b6ef14fe9c1fbc
pedrocauan <mainyasuo92@gmail.com>
2023-04-16 19:41:49 -0300

    primeira rota

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # twitter-clone
 Clone do Twitter feito em NodeJS
+
+
+1. [X] - Primeira Rota. 
--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const app = express();
 const port = 3003;
 
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get("/", (req, res, next) => {
+    res.status(200).send("Yahoo!");
+})
