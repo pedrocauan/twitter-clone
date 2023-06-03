1773de09903c05285034ccf58f4e3fa2a87fb6af
pedrocauan <mainyasuo92@gmail.com>
2023-06-03 20:25:27 -0300

    middleware para login

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Clone do Twitter feito em NodeJS
 1. [X] - Configurando e renderizando a primeira pagina com o Pug.
 1. [X] - Enviar dados para o front end e utilizar o pug para mostrar na tela.
 1. [X] - Herança de elementos com o pug.
+1. [X] - Middleware de login
--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+//imports
 const express = require("express");
-
 const app = express();
+
+//middlewares
+const middleware = require("./middleware")
+
 const port = 3003;
 
+//rota o server
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
+//Configurações
 app.set("view engine", "pug"),
 app.set("views", "views")
 
-app.get("/", (req, res, next) => {
+//rotas
+app.get("/", middleware.requireLogin, (req, res, next) => {
     const payload = {
         pageTitle: "home"
 
